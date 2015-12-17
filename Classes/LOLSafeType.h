0f4edc3ff5e69e26f6da9458981a16d7598c0ae6
Flanker4 <_flanker_4@mail.ru>
2015-12-17 22:41:07 +0200

    Update LOLSafeType.h
    Added swift-like macros

--- Classes/LOLSafeType.h
+++ Classes/LOLSafeType.h
@@ -9,3 +9,22 @@
 
 #define LOLDynamicCast(object,classType) ([(object) isKindOfClass:[classType class]]?((classType*)object):nil)
 
+#define _cast(classType,object) LOLDynamicCast(object, classType)
+
+#define _if_let(classType, var, object) \
+    classType* var = _cast(classType,object);\
+    if  (var)
+
+
+#define _guard(predicate)\
+    if (predicate){}
+
+#define _guard_let(classType, var, object)\
+    classType* var = _cast(classType,object);\
+    _guard(predicate)
+
+#define _safe_block(block,...)  if (block) {block(__VA_ARGS__);}
+
+#define _ui(code) dispatch_async(dispatch_get_main_queue(), ^{ code; });
+#define _ui_block(block,...) _ui( _safe_block(block,__VA_ARGS__) );
+
